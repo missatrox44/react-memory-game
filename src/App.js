7ec92631564444c99b988ccef5d98d816ea96038
Sara Baqla <missatrox44@gmail.com>
2023-04-02 20:35:11 -0500

    shuffle cards function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const cardImages = [
+  { src: "/imgs/chris-bike.jpeg" },
+  { src: "/imgs/chris-repel.jpeg" },
+  { src: "/imgs/jaws.jpeg" },
+  { src: "/imgs/sara-bike.jpeg" },
+  { src: "/imgs/sara-repel.jpeg" },
+  { src: "/imgs/sitting-water.jpeg" },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0)
+
+  //shuffle cards -> create two of each card, mix them up, add ids
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    //update card state
+    setCards(shuffledCards);
+    setTurns(0)
+  };
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
-     <h1>Magic Match</h1>
-     <button>New Game</button>
+      <h1>Rio Secreto Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
